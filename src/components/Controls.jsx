7a04b6c06bfa5034c7ddeec5abdabba8dbc19549
Aaron Storm <julian.storm@testandtrade.com>
2021-08-03 02:07:28 +0700

    added more focus

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -2,11 +2,9 @@
 import { ReactComponent as Power } from "../assets/power.svg";
 import Button from "./Button.jsx";
 import classNames from "classnames";
-import VolumeSlider from "./VolumeSlider";
 import "./Controls.css";
 
 function Controls({ activeBank, handleVolume, theme, power }) {
-
   let wrapperClass = classNames("ctrlWrapper", {
     off: !power,
   });
@@ -15,13 +13,13 @@ function Controls({ activeBank, handleVolume, theme, power }) {
     active: power && activeBank === "bankA",
     darkTheme: theme,
     lightTheme: !theme,
-    hidden: !power
+    hidden: !power,
   });
   let bankBClass = classNames("ctrlBtn bankB", {
     active: power && activeBank === "bankB",
     darkTheme: theme,
     lightTheme: !theme,
-    hidden: !power
+    hidden: !power,
   });
   let powerClass = classNames("ctrlBtn pwr", {
     darkTheme: theme,
@@ -29,32 +27,28 @@ function Controls({ activeBank, handleVolume, theme, power }) {
   });
 
   let inputClass = classNames("volumeWrapper", {
-    off: !power
-  })
+    off: !power,
+  });
 
-  let iconClass = classNames('power', {
+  let iconClass = classNames("power", {
     iconOff: !power,
-
-  })
+  });
 
   const btnProps = [
     {
       className: bankAClass,
       id: "bankA",
       children: <span className="text">BANK A</span>,
-      // tabIndex: !power ? -1 : 0
     },
     {
       className: powerClass,
       id: "pwr",
       children: <Power className={iconClass} />,
-      // tabIndex: 0
     },
     {
       className: bankBClass,
       id: "bankB",
       children: <span className="text">BANK B</span>,
-      // tabIndex: !power ? -1 : 0
     },
   ];
 
@@ -69,7 +63,6 @@ function Controls({ activeBank, handleVolume, theme, power }) {
               children={prop.children}
               power={power}
               theme={theme}
-              tabIndex={prop.tabIndex}
               key={prop.id}
             />
           );
--- src/components/DrumPad.jsx
+++ src/components/DrumPad.jsx
@@ -1,33 +1,40 @@
-import { useState } from 'react'
-import classNames from 'classnames'
+import { useState } from "react";
+import classNames from "classnames";
 import "./DrumPad.css";
 
-function DrumPad({className, theme, id, src, children, power}) {
-
+function DrumPad({ className, theme, id, src, children, power }) {
   const [hover, setHover] = useState(false);
+  const [focus, setFocus] = useState(false);
 
   function handleHover(e) {
     setHover(!hover);
   }
 
+  function handleFocus(e) {
+    setFocus(!focus);
+  }
 
-  let btnClass = classNames( className, {
-    'off': !power,
-    'darkBtn': theme,
-    'lightBtn': !theme,
-    'darkBtnHover': power && hover && theme,
-    'lightBtnHover': power && hover && !theme,
-    'hoverScale': power && hover
-  })
-  
+
+  let btnClass = classNames(className, {
+    off: !power,
+    darkBtn: theme,
+    lightBtn: !theme,
+    darkBtnHover: power && hover && theme,
+    lightBtnHover: power && hover && !theme,
+    hoverScale: !focus && power && hover,
+    darkBtnFocus: power && theme && focus,
+    lightBtnFocus: power && !theme && focus
+  });
 
   return (
-    <button 
-    className={btnClass} 
-    id={id} 
-    onMouseEnter={handleHover}
-    onMouseLeave={handleHover}
-    tabIndex={!power ? -1: 0}
+    <button
+      className={btnClass}
+      id={id}
+      onMouseEnter={handleHover}
+      onMouseLeave={handleHover}
+      onFocus={handleFocus}
+      onBlur={handleFocus}
+      tabIndex={!power ? -1 : 0}
     >
       <audio
         className="clip"

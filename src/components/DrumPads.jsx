f7f8c9fef1e1b6acf4630918829bc11786d12178
Aaron Storm <julian.storm@testandtrade.com>
2021-08-04 01:44:38 +0700

    fixed homepage

--- src/components/DrumPads.jsx
+++ src/components/DrumPads.jsx
@@ -1,22 +1,21 @@
-import realKick from '../assets/audio/bankA/real-life-KICK.wav'
-import realSN1 from '../assets/audio/bankA/real-life-SN1.WAV'
-import realTom1F from '../assets/audio/bankA/real-life-TOM1F.WAV'
-import realTOM1L from '../assets/audio/bankA/real-life-TOM1L.WAV'
-import realTOM2LAMBI from '../assets/audio/bankA/real-life-TOM2LAMBI.WAV'
-import realRIDE from '../assets/audio/bankA/real-life-SN2.WAV'
-import realCRASH1 from '../assets/audio/bankA/real-life-CRASH1.WAV'
-import realCRASH2 from '../assets/audio/bankA/real-life-CRASH2.WAV'
-import realRIM from '../assets/audio/bankA/real-life-RIM.WAV'
-import SharpKICK from '../assets/audio/bankB/analog-sharp-kick.wav'
-import SN from '../assets/audio/bankB/ec-sn022.wav'
-import SLAP from '../assets/audio/bankB/kungfu-slap.wav'
-import TONGUE from '../assets/audio/bankB/tongue-ripple-hit.wav'
-import CLAP from '../assets/audio/bankB/Clap2.wav'
-import STABS from '../assets/audio/bankB/stabs-hits-061.wav'
-import STABS2 from '../assets/audio/bankB/stabs-hits-144.wav'
-import CRASH from '../assets/audio/bankB/909-bright-crash.wav'
-import SteelDrum from '../assets/audio/bankB/SteelDrum-vs.wav'
-
+import realKick from "../assets/audio/bankA/real-life-KICK.wav";
+import realSN1 from "../assets/audio/bankA/real-life-SN1.WAV";
+import realTom1F from "../assets/audio/bankA/real-life-TOM1F.WAV";
+import realTOM1L from "../assets/audio/bankA/real-life-TOM1L.WAV";
+import realTOM2LAMBI from "../assets/audio/bankA/real-life-TOM2LAMBI.WAV";
+import realRIDE from "../assets/audio/bankA/real-life-SN2.WAV";
+import realCRASH1 from "../assets/audio/bankA/real-life-CRASH1.WAV";
+import realCRASH2 from "../assets/audio/bankA/real-life-CRASH2.WAV";
+import realRIM from "../assets/audio/bankA/real-life-RIM.WAV";
+import SharpKICK from "../assets/audio/bankB/analog-sharp-kick.wav";
+import SN from "../assets/audio/bankB/ec-sn022.wav";
+import SLAP from "../assets/audio/bankB/kungfu-slap.wav";
+import TONGUE from "../assets/audio/bankB/tongue-ripple-hit.wav";
+import CLAP from "../assets/audio/bankB/Clap2.wav";
+import STABS from "../assets/audio/bankB/stabs-hits-061.wav";
+import STABS2 from "../assets/audio/bankB/stabs-hits-144.wav";
+import CRASH from "../assets/audio/bankB/909-bright-crash.wav";
+import SteelDrum from "../assets/audio/bankB/SteelDrum-vs.wav";
 
 import "./DrumPads.css";
 import DrumPad from "./DrumPad";
@@ -116,21 +115,22 @@ const BankB = [
   },
 ];
 
-
-
-function DrumPads({ clipName, active, onClick, activeBank, theme, power, audioRef }) {
-
- const targetBank = activeBank === 'bankA'? BankA : BankB 
+function DrumPads({
+  clipName,
+  active,
+  onClick,
+  activeBank,
+  theme,
+  power,
+  audioRef,
+}) {
+  const targetBank = activeBank === "bankA" ? BankA : BankB;
 
   return (
     <div className="drum-pads">
-      {
-      
-      targetBank.map((clip) => {
+      {targetBank.map((clip) => {
         const className =
-          clip.id === clipName && active
-            ? 'drum-pad playing'
-            : 'drum-pad';
+          clip.id === clipName && active ? "drum-pad playing" : "drum-pad";
         return (
           <DrumPad
             className={className}
@@ -142,7 +142,6 @@ function DrumPads({ clipName, active, onClick, activeBank, theme, power, audioRe
             theme={theme}
             power={power}
             audioRef={audioRef}
-            
           />
         );
       })}
